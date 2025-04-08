2daf67bb96437b1ca3607c78585f90288a8db385
siddharthCodesss <singhsiddharth3598@gmail.com>
2025-04-09 01:28:13 +0530

    Created Splash View, and architecture

---
+++ lib/presentation/splash/view/splash_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.amber,
+      ),
+    );
+  }
+}
